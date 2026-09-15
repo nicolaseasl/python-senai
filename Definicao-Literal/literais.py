46dbcfb689e0fbbf6164f43352896c794d356273
nicolaseasl <nicolas20081019@gmail.com>
2026-09-15 14:29:44 -0300

    Adicionando literais

---
+++ Definicao-Literal/literais.py
@@ -0,0 +1,15 @@
+# Strint 
+nome = "Nicolas"
+
+# Números inteiros e decimais.
+idade = 17
+altura = 1.65
+
+# Valor lógico (true e False)
+professor = False
+
+# Verifica a tipagem das variáveis
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(professor))
