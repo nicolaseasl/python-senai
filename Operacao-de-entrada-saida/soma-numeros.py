412f189f48458202e082d36d8516d7b21f6dc521
nicolaseasl <nicolas20081019@gmail.com>
2026-09-17 14:01:49 -0300

    Criando inputs e fazendo calculos

---
+++ Operacao-de-entrada-saida/soma-numeros.py
@@ -0,0 +1,15 @@
+# Entrada de dados básicos
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+# Processamento computacional
+soma = numero1 + numero2
+sub = numero1 - numero2
+div = numero1 / numero2
+mult = numero1 * numero2
+
+# Saída da informação
+print(f"A soma é: {soma}")
+print(f"A subtração é: {sub}")
+print(f"A divisão é: {div}")
+print(f"A multiplicação é: {mult}")
