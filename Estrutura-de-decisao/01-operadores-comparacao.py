82a604ba4123305e8d9cc5f8f3cdec4a75bdd054
nicolaseasl <nicolas20081019@gmail.com>
2026-09-18 14:09:11 -0300

    Adicionando formas booleanicas

--- Estrutura-de-decisao/01-operadores-comparacao.py
+++ Estrutura-de-decisao/01-operadores-comparacao.py
@@ -5,9 +5,19 @@ numero2 = int(input("Digite outro número: "))
 # Exibe o resultado das expressões
 
 # Verifica se os números são iguais
-print("num1 == num2: ", numero1 == numero2)
+print("numero1 == numero2: ", numero1 == numero2)
 
 # Verifica se os números são diferentes
-print("num1 != num2: ", numero1 != numero2)
+print("numero1 != numero2: ", numero1 != numero2)
 
-# Verifica se os números são menores
+# Verifica se o numero1 é menor que numero2
+print("numero1 < numero2: ", numero1 < numero2)
+
+# Verifia se o numero1 é menor ou igual a numero2
+print("numero1 <= numero2: ", numero1 <= numero2)
+
+# Verifica se o numero1 é maior que numero2
+print("numero1 > numero2: ", numero1 > numero2)
+
+# Verifia se o numero1 é maior ou igual a numero2
+print("numero1 >= numero2: ", numero1 >= numero2)
